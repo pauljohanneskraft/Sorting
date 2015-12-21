ccf5ee69082354a6382ecbc6d98ea4d33390cb4a
Paul <pauljohanneskraft@Pauls-MacBook.local>
2015-12-21 14:44:55 +0100

    several changes

--- Swift/Sort/Sort/main.swift
+++ Swift/Sort/Sort/main.swift
@@ -1,9 +1,47 @@
-import Cocoa
 
-// main.swift
-// Created by Paul Kraft on 04.12.15.
+//
+//  main.swift
+//  Sort
+//
+//  Created by Paul Kraft on 13.12.15.
+//  Copyright © 2015 Paul Kraft. All rights reserved.
+//
 
-for i in 0...0 {
-    var s = UseCase(rounds:1,elements:1000000,showResultsOfRound:true,showAllArrays:false)
+let              times = 1
+let             rounds = 10
+let           elements = 1000
+let showResultsOfRound = false
+let      showAllArrays = false
+
+let sortingAlgorithms : [[Number] -> (String, [Number])] = [
+    //
+    shellSort,
+    //mergeSort,
+    //mergeSortThreaded,
+    //selectionSort,
+    //insertionSort,
+    //quickSortFirst,
+    //quickSortFirstThreaded,
+    //quickSortInPlace,
+    //quickSortInPlaceThreaded,
+    //quickSortMedian,
+    //quickSortMedianThreaded,
+    //quickSortRandom,
+    //
+    quickSortRandomThreaded,
+    //
+    introSortMedianThreaded,
+    //introSortMedian,
+    //bubbleSort
+    //
+    binaryTreeSort
+]
+
+for i in 0..<times {
+    var s = Test(
+        rounds:rounds,
+        elements:elements,
+        showResultsOfRound:showResultsOfRound,
+        showAllArrays:showAllArrays,
+        sortingAlgorithms: sortingAlgorithms)
 }
-
