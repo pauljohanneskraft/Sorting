7e1697fdea0a828ce2fcfd7a8cf8da0ec0f064e6
Paul Kraft <pauljohanneskraft@244.217.eduroam.dynamic.rbg.tum.de>
2016-04-06 14:34:50 +0200

    updated Swift to ignore project file

---
+++ Swift/Sorting/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sorting
+//
+//  Created by Paul Kraft on 06.04.16.
+//  Copyright © 2016 pauljohanneskraft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
