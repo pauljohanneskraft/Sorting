fbb5a0f5fa3590b34671bfa6c128c5c0a5b850c5
Paul Kraft <pauljohanneskraft@Pauls-MacBook.fritz.box>
2016-04-09 23:16:08 +0200

    SelectionSort added

--- C++/Sorting/sorting algorithms/SelectionSort.cpp
+++ C++/Sorting/sorting algorithms/SelectionSort.cpp
@@ -7,3 +7,21 @@
 //
 
 #include "SelectionSort.hpp"
+
+SelectionSort::SelectionSort() : Sort("SelectionSort") {}
+
+void SelectionSort::sort(Comparable *array[], int length) {
+    sort(array, 0, length -1);
+}
+
+void SelectionSort::sort(Comparable *array[], int left, int right) {
+    for(int i = left; i <=right; i++) {
+        int min = i;
+        for(int j = i+1; j <= right; j++) {
+            if(*array[j] < array[min]) { min = j; }
+        }
+        Comparable *tmp = array[i];
+        array[i] = array[min];
+        array[min] = tmp;
+    }
+}
--- C++/Sorting/sorting algorithms/SelectionSort.hpp
+++ C++/Sorting/sorting algorithms/SelectionSort.hpp
@@ -10,5 +10,14 @@
 #define SelectionSort_hpp
 
 #include <stdio.h>
+#include "Sort.hpp"
+
+class SelectionSort : public Sort {
+public:
+    SelectionSort();
+    void sort(Comparable *array[], int length);
+protected:
+    void sort(Comparable *array[], int left, int right);
+};
 
 #endif /* SelectionSort_hpp */
