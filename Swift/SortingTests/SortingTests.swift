5b9c87c35c67fb466539efaa7d6d11b8acb94501
Paul <pauljohanneskraft@users.noreply.github.com>
2016-08-03 14:45:03 +0200

    added output for avg elementCount, check for count, some algorithm tests, improved random elementCount

--- Swift/SortingTests/SortingTests.swift
+++ Swift/SortingTests/SortingTests.swift
@@ -14,12 +14,14 @@ class SortingTest : XCTestCase {
     
     var elements : [[N]] = []
     
-    var arrayCount              = 200
-    var elementCount : UInt32   = 1_000
-    
     override func setUp() {
+        self.setUp(arrayCount: 200, elementCount: 100)
+    }
+    
+    func setUp(arrayCount: Int, elementCount: UInt32) {
+        elements = []
         for _ in 0..<arrayCount {
-            let count = arc4random() % elementCount + 5
+            let count = (arc4random() % (elementCount)) + (elementCount / 2)
             var array = [Int]()
             for _ in 0..<count {
                 array.append(Int(arc4random() % UInt32(elementCount * 10)))
@@ -27,19 +29,44 @@ class SortingTest : XCTestCase {
             elements.append(array)
         }
     }
+
     
-    func testRadixSort()        { forAll("RadixSort"        ) { $0.radixSort()          } }
-    func testRadixSortInPlace() { forAll("RadixSortInPlace" ) { $0.radixSortInPlace()   } }
-    func testSelectionSort()    { forAll("SelectionSort"    ) { $0.selectionSort()      } }
-    func testInsertionSort()    { forAll("InsertionSort"    ) { $0.insertionSort()      } }
-    func testIntroSort()        { forAll("IntroSort"        ) { $0.introSort()          } }
-    func testShellSort()        { forAll("ShellSort"        ) { $0.shellSort()          } }
-    func testMergeSort()        { forAll("MergeSort"        ) { $0.mergeSort()          } }
-    func testQuickSort()        { forAll("QuickSort"        ) { $0.quickSort()          } }
-    func testSwiftSort()        { forAll("Swift"            ) { $0.sort()               } }
-    func testBinaryTreeSort()   { forAll("BinaryTreeSort"   ) { $0.binaryTreeSort()     } }
+    func testBubbleSort()       { forAll("BubbleSort       ") { $0.bubbleSort()         } }
+    func testRadixSort()        { forAll("RadixSort        ") { $0.radixSort()          } }
+    func testRadixSortInPlace() { forAll("RadixSortInPlace ") { $0.radixSortInPlace()   } }
+    func testSelectionSort()    { forAll("SelectionSort    ") { $0.selectionSort()      } }
+    func testInsertionSort()    { forAll("InsertionSort    ") { $0.insertionSort()      } }
+    func testIntroSort()        { forAll("IntroSort        ") { $0.introSort()          } }
+    func testShellSort()        { forAll("ShellSort        ") { $0.shellSort()          } }
+    func testMergeSort()        { forAll("MergeSort        ") { $0.mergeSort()          } }
+    func testQuickSort()        { forAll("QuickSort        ") { $0.quickSort()          } }
+    func testSwiftSort()        { forAll("Swift            ") { $0.sort()               } }
+    func testBinaryTreeSort()   { forAll("BinaryTreeSort   ") { $0.binaryTreeSort()     } }
+    
+    func testSlowOnes()         {
+        let arrayCount = 10
+        let elementCount : UInt32 = 500
+        setUp(arrayCount: arrayCount, elementCount: elementCount)
+        testBubbleSort()
+        testInsertionSort()
+        testSelectionSort()
+        print("." * arrayCount)
+    }
+    
+    func testMediumOnes() {
+        let arrayCount = 10
+        let elementCount : UInt32 = 5_000
+        setUp(arrayCount: arrayCount, elementCount: elementCount)
+        testRadixSort()
+        testShellSort()
+        testMergeSort()
+        print("." * arrayCount)
+    }
     
     func testFastOnes()         {
+        let arrayCount = 10
+        let elementCount : UInt32 = 10_000
+        setUp(arrayCount: arrayCount, elementCount: elementCount)
         // testRadixSort()
         testRadixSortInPlace()
         testQuickSort()
@@ -47,24 +74,49 @@ class SortingTest : XCTestCase {
         testBinaryTreeSort()
         // testShellSort()
         testSwiftSort()
+        print("." * arrayCount)
+    }
+    
+    func testAll() {
+        testSlowOnes()
+        testMediumOnes()
+        testFastOnes()
     }
     
     func forAll(_ desc: String = "Test", _ f: (inout [N]) -> ()) {
         var time = 0.0
-        print()
+        var elementCount = 0
+        // print()
         for es in elements {
             var sorted = es
             let start = NSDate()
             f(&sorted)
             let _time = -start.timeIntervalSinceNow
-            if !sorted.isSorted {
+            elementCount += sorted.count
+            if !sorted.isSorted || sorted.count != es.count {
                 print(sorted)
+                let ess = es.sorted()
+                print(ess)
+                for i in 0..<min(es.count, sorted.count) {
+                    if ess[i] != sorted[i] { print(i, ":", ess[i], "!=", sorted[i]) }
+                }
                 assert(false)
             }
             time += _time
             print(".", terminator: "")
         }
-        print("\n\n\(desc):", elements.count, "tests successful.", "total time:", time, "per test:", time / Double(elements.count), "\n")
+        print("\n")
+        print(
+            "\(desc):",
+            elements.count, "tests with an average of", elementCount / elements.count, "elements were successful.",
+            "total time:", String(format:"%.5f", time),
+            "-> per test:", String(format:"%.5f", time / Double(elements.count)))
+        print()
     }
+}
 
+func * (lhs: String, rhs: Int) -> String {
+    var result = ""
+    for _ in 0..<rhs { result += lhs }
+    return result
 }
